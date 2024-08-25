d94fe65dd8c158765cbd28c235b19852bb445293
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-25 16:02:12 -0700

    Add various install instructions + npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,50 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      // For fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
       /*
       // For images referenced in our HTML template
       npm install --save-dev html-loader
       {
         test: /\.html$/i,
         loader: "html-loader",
+      },
+      // For data files (CSV, TSV, XML)
+      npm install --save-dev csv-loader xml-loader
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      // For toml, yaml, and json5
+      npm install toml yamljs json5 --save-dev
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
       },
       */
     ],
