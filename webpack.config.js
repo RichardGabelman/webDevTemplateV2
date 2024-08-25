d21815144d043272620ab78084d6ad81db092541
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-25 15:57:22 -0700

    Add asset/resource rule

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,19 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      // For images we use in JavaScript
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      /*
+      // For images referenced in our HTML template
+      npm install --save-dev html-loader
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      */
     ],
   },
 };
